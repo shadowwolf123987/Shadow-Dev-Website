5e95bf191a40954dc903399b39eaa9ef3ab0a055
Oli Taylor <oliver.0t.taylor@gmail.com>
2024-03-12 09:59:16 +0000

    Changed Projects Page and fixing image formatting

--- pages/projects.php
+++ pages/projects.php
@@ -12,30 +12,42 @@
 		</div>
 
 		<div class="projectsGrid">
+
 			<div class="projectsDiv">
-				<img src="./imgs/projects/SalesAppIcon.png">
-				<p>This page shows my ASP.NET Web App created for Defensor Life Safety Systems</p>
+				<div class="imgDiv">
+					<img src="./imgs/projects/SalesAppHome.png">
+				</div>
+				<p>This is an ASP.NET Quote Creation tool custom made for Defensor Life Safety Systems</p>
 			</div>
+
 			<div class="projectsDiv">
-				<img src="./imgs/wolfWhite.png">
-				<p>Interested in the various projects I've worked on?<br>The button below will take you to my projects page</p>
+				<div class="imgDiv">
+					<img src="./imgs/projects/SalesAppIcon.png">
+				</div>
+				<p>Certificate Creator Tool</p>
 			</div>
+
 			<div class="projectsDiv">
-				<img src="./imgs/wolfWhite.png">
-				<p>Interested in the various projects I've worked on?<br>The button below will take you to my projects page</p>
+				<div class="imgDiv">
+					<img src="./imgs/projects/.png">
+				</div>
+				<p>Air Gun Range</p>
 			</div>
+
 			<div class="projectsDiv">
-				<img src="./imgs/wolfWhite.png">
-				<p>Interested in the various projects I've worked on?<br>The button below will take you to my projects page</p>
+				<div class="imgDiv">
+					<img src="./imgs/projects/GunQuizGuess.png">
+				</div>
+				<p>Gun Quiz</p>
 			</div>
+
 			<div class="projectsDiv">
-				<img src="./imgs/wolfWhite.png">
-				<p>Interested in the various projects I've worked on?<br>The button below will take you to my projects page</p>
+				<div class="imgDiv">
+					<img src="./imgs/projects/GamblingGameGameplay.png">
+				</div>
+				<p>Python Gambling Game</p>
 			</div>
-			<div class="projectsDiv">
-				<img src="./imgs/wolfWhite.png">
-				<p>Interested in the various projects I've worked on?<br>The button below will take you to my projects page</p>
-			</div>
+
 		</div>
 
     </div>
