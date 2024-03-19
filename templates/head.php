5013576d03ed2a3b697ee56654dc5e0ac1610152
Oli Taylor <oliver.0t.taylor@gmail.com>
2024-03-19 16:15:32 +0000

    Added Google Analytics Tag

--- templates/head.php
+++ templates/head.php
@@ -11,4 +11,14 @@
 	<link href="https://fonts.googleapis.com/css2?family=Ysabeau:wght@200&display=swap" rel="stylesheet">
 	<link rel="apple-touch-icon" href="./imgs/wolfWhite.png">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-3L9YK1ES6L"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'G-3L9YK1ES6L');
+	</script>
 </head>
