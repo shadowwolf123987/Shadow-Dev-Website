008f114444f2b525d385dc765bc982b87437e200
Oli Taylor <oliver.0t.taylor@gmail.com>
2024-03-21 12:01:13 +0000

    Fixed Projects Page Image File Path

--- pages/projects.php
+++ pages/projects.php
@@ -14,42 +14,42 @@
 
 				<div class="projectsDiv" onclick="window.location.href='./projects/SalesApp'">
 					<div class="projectsImgDiv">
-						<img src="./imgs/projects/Sales App/home.png">
+						<img src="./imgs/projects/SalesApp/home.png">
 					</div>
 					<p>This is an ASP.NET Quote Creation tool custom made for Defensor Life Safety Systems</p>
 				</div>
 
 				<div class="projectsDiv" onclick="window.location.href='./projects/WebsterAndHorsfall'">
 					<div class="projectsImgDiv">
-						<img src="./imgs/projects/WH Group/home.png">
+						<img src="./imgs/projects/WHGroup/home.png">
 					</div>
 					<p>Webster and Horsfall Group Website</p>
 				</div>
 
 				<div class="projectsDiv">
 					<div class="projectsImgDiv" onclick="window.location.href='./projects/CertificateCreator'">
-						<img src="./imgs/projects/Certificate Creator/home.png">
+						<img src="./imgs/projects/CertificateCreator/home.png">
 					</div>
 					<p>Certificate Creator Tool</p>
 				</div>
 
 				<div class="projectsDiv">
 					<div class="projectsImgDiv" onclick="window.location.href='./projects/AirgunRange'">
-						<img src="./imgs/projects/Air Gun/home.png">
+						<img src="./imgs/projects/AirGun/home.png">
 					</div>
 					<p>Air Gun Range</p>
 				</div>
 
 				<div class="projectsDiv">
 					<div class="projectsImgDiv" onclick="window.location.href='./projects/GunQuiz'">
-						<img src="./imgs/projects/Gun Quiz/guess.png">
+						<img src="./imgs/projects/GunQuiz/guess.png">
 					</div>
 					<p>Gun Quiz</p>
 				</div>
 
 				<div class="projectsDiv">
 					<div class="projectsImgDiv" onclick="window.location.href='./projects/GamblingGame'">
-						<img src="./imgs/projects/Gambling Game/gameplay.png">
+						<img src="./imgs/projects/GamblingGame/gameplay.png">
 					</div>
 					<p>Python Gambling Game</p>
 				</div>
